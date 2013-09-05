d8daa3c78054734d7b27e10955ea7d9518e3678f
John Gardner <jgardner@transcendcomputing.com>
2013-09-05 09:08:03 -0500

    Update to incorporate fixed bug in evalhook.

--- harp-runtime.gemspec
+++ harp-runtime.gemspec
@@ -22,6 +22,7 @@ Gem::Specification.new do |gem|
   gem.add_runtime_dependency "fog"
   gem.add_runtime_dependency "logging"
   gem.add_runtime_dependency "rgl"
+  gem.add_runtime_dependency "evalhook", '> 0.5.2'
 
   gem.add_development_dependency "rspec"
   gem.add_development_dependency "shotgun"
--- lib/harp-runtime/resourcer.rb
+++ lib/harp-runtime/resourcer.rb
@@ -19,7 +19,7 @@ module Harp
     def consume(template_or_uri)
       # TODO: handle URL
       @@logger.info "Consume template: #{template_or_uri.length.to_s}"
-      #JSON.parse(template_or_uri)
+      JSON.parse(template_or_uri)
     end
 
     # Create a resource and wait for the resource to become available.
