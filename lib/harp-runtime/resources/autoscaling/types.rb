1d3bb5d0795cab04e161dcac1abf77acd719e3c6
Nestor <nalmanza@gmail.com>
2013-12-02 13:34:20 -0600

    Added scaling policy resource

--- lib/harp-runtime/resources/autoscaling/types.rb
+++ lib/harp-runtime/resources/autoscaling/types.rb
@@ -2,6 +2,8 @@ require 'set'
 
 require 'harp-runtime/resources/autoscaling/launch_configuration'
 require 'harp-runtime/resources/autoscaling/autoscaling_group'
+require 'harp-runtime/resources/autoscaling/scaling_policy'
+
 
 module Harp
   module Resources
