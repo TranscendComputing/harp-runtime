a660568d8ff05532fc8ccf8642fcbeb74c2934d0
Nestor <nalmanza@gmail.com>
2013-12-10 11:46:08 -0600

    Added networkInterface, subnet resources

--- lib/harp-runtime/resources/compute/types.rb
+++ lib/harp-runtime/resources/compute/types.rb
@@ -3,7 +3,9 @@ require 'set'
 require 'harp-runtime/resources/compute/elastic_ip'
 require 'harp-runtime/resources/compute/elastic_ip_association'
 require 'harp-runtime/resources/compute/instance'
+require 'harp-runtime/resources/compute/network_interface'
 require 'harp-runtime/resources/compute/security_group'
+require 'harp-runtime/resources/compute/subnet'
 require 'harp-runtime/resources/compute/volume'
 
 
